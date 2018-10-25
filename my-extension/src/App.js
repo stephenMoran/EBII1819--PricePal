865a8ed126bb35084c6aad194e9006187e545497
stephenMoran <32431967+stephenMoran@users.noreply.github.com>
2018-10-25 14:30:47 +0100

    Reacy fix

--- my-extension/src/App.js
+++ my-extension/src/App.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Price from './components/Price'
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import BestPrice from './components/BestPrice'
 
 class App extends Component {
 
   constructor() {
           super()
           this.state = {
-
+              prices: [],
+              bestPrice: null
           }
       }
+
+  componentDidMount() {
+
+
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> Hello </h1>
+        <BestPrice />
       </div>
     );
   }
--- my-extension/src/components/BestPrice.js
+++ my-extension/src/components/BestPrice.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 class BestPrice extends React.Component {
-
-
+  render() {
+        return (
+            <div className="best-price">
+                <h1>€71.99</h1>
+            </div>
+        )
+    }
 }
 export default BestPrice
