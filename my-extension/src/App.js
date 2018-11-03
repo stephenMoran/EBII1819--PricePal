f786c34bb03895c613d6e73f2dde7aefa371023e
CalumCo <cacomerf>
2018-11-03 12:06:50 +0000

    Update my-extension/src/App.js

--- my-extension/src/App.js
+++ my-extension/src/App.js
@@ -10,20 +10,37 @@ class App extends Component {
           super()
           this.state = {
               prices: [],
-              bestPrice: null
+              bestPrice: null,
+              size : 3
           }
       }
 
   componentDidMount() {
 
-
   }
 
   render() {
-    return (
-      <div className="App">
-        <h1> Hello </h1>
-        <BestPrice />
+    let rows = [];
+    for (var i = 0; i < this.state.size; i++){
+      let rowID = `row${i}`
+      let cell = []
+      for (var idx = 0; idx < this.state.size; idx++){
+        let cellID = 'cell${i}-${idx}'
+        cell.push(<td key={cellID} id={cellID}></td>)
+    }
+    rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+  }
+  return (
+    <div className="container">
+        <div className="row">
+          <div className="col s12 board">
+            <table id="simple-board">
+               <tbody>
+                 {rows}
+               </tbody>
+             </table>
+          </div>
+        </div>
       </div>
     );
   }
