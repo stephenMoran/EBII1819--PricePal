c4a474769a7c08042a1998bfd39eb40db5621dc5
Daniel Connaughton <connauda@tcd.ie>
2018-11-07 16:44:34 +0000

    Added country flags and links to local sites

--- my-extension/src/components/PriceList.js
+++ my-extension/src/components/PriceList.js
@@ -14,22 +14,34 @@ class PriceList extends React.Component {
               <p>Purchasing from the UK saves you €1!</p>
               <ul>
                 <li>
-                  <a>UK</a>
+                  <a href="https://www.amazon.co.uk" target="_blank">
+                    <img src=".\imgs\UK.png" alt="UK"/>
+                    &nbsp;UK
+                  </a>
                   <a2>€38 list price</a2>
                   <a3>€40</a3>
                 </li>
                 <li>
-                  <a>ES</a>
+                  <a href="https://www.amazon.de" target="_blank">
+                    <img src=".\imgs\DE.png" alt="DE"/>
+                    &nbsp;DE
+                  </a>
                   <a2>€38 list price</a2>
                   <a3>€41</a3>
                 </li>
                 <li>
-                  <a>FR</a>
+                  <a href="https://www.amazon.es" target="_blank">
+                    <img src=".\imgs\ES.png" alt="ES"/>
+                    &nbsp;ES
+                  </a>
                   <a2>€44 list price</a2>
                   <a3>€46</a3>
                 </li>
                 <li>
-                  <a>DE</a>
+                  <a href="https://www.amazon.fr" target="_blank">
+                    <img src=".\imgs\FR.png" alt="FR"/>
+                    &nbsp;FR
+                  </a>
                   <a2>€55 list price</a2>
                   <a3>€67</a3>
                 </li>
