53cb17b871a0c0d338f76757891d3b99b4d742a4
Gabriel <35768797+Gabe1704@users.noreply.github.com>
2018-11-12 21:26:34 +0000

    Added some clauses

--- my-extension/src/utils/Shipping.js
+++ my-extension/src/utils/Shipping.js
@@ -1,10 +1,10 @@
-function getShippingPrices(weight, country)
+function getShippingPrices(weight, country, shippingCategory, priceofItem)
     {
 
 
     //console.log(weight);
 
-    var countries = ['UK','Spain', 'France','Italy', 'Germany', 'Ireland'];
+    var countries = ['uk','es', 'fr','it', 'de', 'ire'];
 
 
     var shippingBMDSV = [ [2.99 , 4.49, 3.49, 4.49, 3.49 , 4.00], [2.99, 0.99, 3.99, 4.99, 3.99, 5.00], [2.99, 4.99, 2.79, 4.99, 3.99, 7.50], [2.99, 4.99, 3.99, 2.70, 3.99, 5.00], [2.99, 4.99, 3.99, 4.99, 0, 5.00] ];
@@ -12,15 +12,69 @@ function getShippingPrices(weight, country)
     var shippingCosts = [ [4.49, 4.99, 3.99, 5.49, 3,49, 6.00], [3.99, 2.99, 4.49, 5.99, 3.99, 7.50], [3.99, 5.49, 4.99, 5.99, 3.99, 7.50], [3.99, 5.49, 4.49, 3.99, 3.99, 7.50], [3.99, 5.49, 4.49, 5.99, 5.00, 7.50] ];
     var costPerKG =  [ [0, 1.99, 1.49, 0.99, 0.99, 0], [0.99, 0, 1.49, 0.99, 0.99, 1.30], [0.99, 1.99, 0, 0.99, 0.99, 1.30], [0.99, 1.99, 1.49, 0, 0.99, 1.30], [0.99, 1.99, 1.49, 0.99, 0, 1.30] ];
 
-
+    var countryNum;
+    for( x =0; x < countries.length ; x++){
+      if(countries[x]==country ){
+        countryNum = x;
+      }
+      else{
+        countryNum = 0;
+      }
+    }
 
     var prices = new Array(5);
-    var i =0;
-    for( i =0 ; i < countries.length; i++)
-    {
-        prices[i] = shippingCosts[i][country];
-        prices[i] += costPerKG[i][country]*weight;
-        //console.log(prices[i]);//
+    if(shippingCategory == "BMDSV"){
+      for( i =0 ; i < countries.length - 1; i++)
+      {
+          prices[i] = shippingBMDSV[i][countryNum];
+          prices[i] += costPerKG[i][countryNum]*weight;
+          //console.log(prices[i]);//
+          if((i==0 && countryNum==0)&&priceofItem>=10){
+            prices[0]=0.00;
+          }
+          if((i==0 && countryNum==5)&&priceofItem>=25){
+            prices[0]=0.00;
+          }
+          if((i==1 && countryNum==1)&&priceofItem>=19){
+            prices[0]=0.00;
+          }
+          if((i==2 && countryNum==2)&&priceofItem>=25){
+            prices[0]=0.00;
+          }
+          if((i==3 && countryNum==3)&&priceofItem>=25){
+            prices[0]=0.00;
+          }
+          if((i==4 && countryNum==4)&&priceofItem>=0){
+            prices[0]=0.00;
+          }
+
+        }
+    }
+    else{
+      for( i =0 ; i < countries.length - 1; i++)
+      {
+          prices[i] = shippingCosts[i][countryNum];
+          prices[i] += costPerKG[i][countryNum]*weight;
+          //console.log(prices[i]);//
+          if((i==0 && countryNum==0)&&priceofItem>=20){
+            prices[0]=0.00;
+          }
+          if((i==0 && countryNum==5)&&priceofItem>=25){
+            prices[0]=0.00;
+          }
+          if((i==1 && countryNum==1)&&priceofItem>=29){
+            prices[0]=0.00;
+          }
+          if((i==2 && countryNum==2)&&priceofItem>=25){
+            prices[0]=0.00;
+          }
+          if((i==3 && countryNum==3)&&priceofItem>=25){
+            prices[0]=0.00;
+          }
+          if((i==4 && countryNum==4)&&priceofItem>=29){
+            prices[0]=0.00;
+          }
+        }
     }
     return(prices);
 
